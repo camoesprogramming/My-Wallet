e44371a27caa8977979ebf44a500fb527086ba2c
camoesprogramming <camoesprogramming@gmail.com>
2023-10-23 18:13:53 -0300

    AuthContext implementado para poder guardar o token do usuário

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,20 @@ import SignUp from "./Pages/SignUp";
 import Home from "./Pages/Home";
 import NewIncome from "./Pages/NewIncome";
 import NewExpense from "./Pages/NewExpense";
-
+import AuthenticationProvider from "./Contexts/AuthContext"
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route  path="/" element = {<SignIn />}/>
-      <Route  path="/signUp" element = {<SignUp />}/>
-      <Route  path="/home" element = {<Home />} />
-      <Route path="/new-income" element = {<NewIncome />} />
-      <Route path="/new-expense" element = {<NewExpense />} />
-    </Routes>
+      <AuthenticationProvider>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/new-income" element={<NewIncome />} />
+          <Route path="/new-expense" element={<NewExpense />} />
+        </Routes>
+      </AuthenticationProvider>
     </BrowserRouter>
   );
 }
