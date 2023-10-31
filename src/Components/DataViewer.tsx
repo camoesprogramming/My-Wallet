c2311264e52f2fd287c5d498c8b8e359fe2bb040
Alisson Camoes Araujo <camoesprogramming@Alissons-Mac-mini.local>
2023-10-31 19:44:19 -0300

    icones de x implmentado no componente DataViewer e estrutura de dados q vão ser obtidos pelo backend levados em consideração na hora de renderizar

--- src/Components/DataViewer.tsx
+++ src/Components/DataViewer.tsx
@@ -1,72 +1,77 @@
-import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 type Data = {
-  date: string,
-  description: string,
-  value: number,
-  type: string
-}
+  date: string;
+  description: string;
+  value: string;
+  income: string;
+  id: string;
+};
 
 export default function DataViewer() {
-
-  
-
   let DataExample: Data[] = [
     {
-      date:"30/11",
+      date: "30/11",
       description: "Almoço com mãe",
-      value: 45.90,
-      type:"saida"
+      value: "45.90",
+      income: "false",
+      id: "1",
     },
     {
       date: "02/12",
       description: "Compras de supermercado",
-      value: 120.50,
-      type:"entrada",
+      value: "120.50",
+      income: "true",
+      id: "2",
     },
     {
       date: "15/12",
       description: "Jantar com amigos",
-      value: 75.00,
-      type:"saida"
+      value: "75.00",
+      income: "false",
+      id: "3",
     },
     {
       date: "20/12",
       description: "Gasolina",
-      value: 60.30, 
-      type:"entrada"
-    },    
+      value: "60.30",
+      income: "true",
+      id: "4",
+    },
     {
       date: "25/12",
       description: "Presentes de Natal",
-      value: 200.00,
-      type:"saida"
-    }
-  ] 
-
+      value: "200.00",
+      income: "false",
+      id: "5",
+    },
+  ];
 
   return (
-    <Container justify= {DataExample ? "start" : "center"}>
-      {!DataExample && (
-        <h1>Não há registros de entrada ou saída</h1>
-      ) 
-      }
-      
-      
+    <Container justify={DataExample ? "start" : "center"}>
+      {!DataExample && <h1>Não há registros de entrada ou saída</h1>}
+
       {DataExample.map((e) => (
-        <StyledData color={e.type === "entrada" ? "#03AC00" : "#C70000" } key={e.date}>
-          <p><span>{e.date}</span> {e.description}</p>
-          <p>{e.value}</p>
+        <StyledData
+          color={e.income === "true" ? "#03AC00" : "#C70000"}
+          key={e.id}
+        >
+          <p>
+            <span>{e.date}</span> {e.description}
+          </p>
+          <div>
+            <p>{e.value.replace(".", ",")}</p>
+            <FontAwesomeIcon icon={faXmark} size="2xs" style={{color: "#000000"}} />
+          </div>
         </StyledData>
       ))}
-      
-
     </Container>
   );
 }
 
-const Container = styled.div<{justify: string}>`
+const Container = styled.div<{ justify: string }>`
   margin: auto;
   margin-top: 28px;
   width: 90%;
@@ -76,7 +81,7 @@ const Container = styled.div<{justify: string}>`
 
   display: flex;
   flex-direction: column;
-  justify-content: ${props => props.justify};
+  justify-content: ${(props) => props.justify};
   align-items: center;
 
   h1 {
@@ -90,16 +95,16 @@ const Container = styled.div<{justify: string}>`
   }
 `;
 
-const StyledData = styled.div<{color: string}>`
-  display:flex;
+const StyledData = styled.div<{ color: string }>`
+  display: flex;
   justify-content: space-between;
   align-items: center;
   width: 95%;
   background-color: #fff;
   margin-top: 12px;
-  
+
   p {
-    font-family: 'Raleway';
+    font-family: "Raleway";
     font-size: 16px;
     font-weight: 400;
     line-height: 19px;
@@ -110,13 +115,23 @@ const StyledData = styled.div<{color: string}>`
   }
 
   span {
-    color: #C6C6C6;
+    color: #c6c6c6;
     background-color: #fff;
     padding-right: 20px;
   }
- 
-  p:last-of-type {
-    color: ${props => (props.color)};
-  }
 
-`
+  
+  div {
+    background-color: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    p {
+      color: ${(props) => props.color};
+      margin-right: 8px;
+    }
+
+
+  }
+`;
