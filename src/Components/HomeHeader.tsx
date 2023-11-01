b48bbcb78becc2ab3e6e7565e413806a75b370bd
Alisson Camoes Araujo <camoesprogramming@Alissons-Mac-mini.local>
2023-11-01 11:45:53 -0300

    link de navegação implementado para botão de logout

--- src/Components/HomeHeader.tsx
+++ src/Components/HomeHeader.tsx
@@ -1,17 +1,21 @@
-import styled from "styled-components"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
-
-
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 export default function HomeHeader() {
   return (
     <Container>
       <h1>Olá, fulano!</h1>
-      <FontAwesomeIcon icon={faRightFromBracket} size="2xl" style={{color: "#ffffff",}} />
-      
+      <Link to="/">
+        <FontAwesomeIcon
+          icon={faRightFromBracket}
+          size="2xl"
+          style={{ color: "#ffffff" }}
+        />
+      </Link>
     </Container>
-  )
+  );
 }
 
 const Container = styled.div`
@@ -21,15 +25,14 @@ const Container = styled.div`
   justify-content: space-between;
   align-items: center;
   margin-top: 25px;
-  
+
   h1 {
-    font-family: 'Raleway';
+    font-family: "Raleway";
     font-size: 26px;
     font-weight: 700;
     line-height: 31px;
     letter-spacing: 0em;
     text-align: left;
-    color: #FFFFFF;
+    color: #ffffff;
   }
-  
-`
+`;
